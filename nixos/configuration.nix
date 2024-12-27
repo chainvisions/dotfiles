e511160b29c111e7c5e84ba1694614e5dcc84ae3
chainvisions <chainvisions@outlook.com>
2024-12-27 02:12:30 -0700

    Add i18n config for nixfiles

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -15,6 +15,18 @@
 
   # Time & Locale.
   time.timeZone = "America/Denver";
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHOEN = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
 
   # Program Options.
   programs.yazi.enable = true;
